999e09267a113a4c34359b8b2d709a29214ab560
amegane <amegane3231@gmail.com>
2022-02-22 23:27:31 +0900

    Delete unnecessary file

--- shared/src/commonMain/kotlin/com/amegane3231/videosearcher/Greeting.kt
+++
@@ -1,7 +0,0 @@
-package com.amegane3231.videosearcher
-
-class Greeting {
-    fun greeting(): String {
-        return "Hello, ${Platform().platform}!"
-    }
-}
--- shared/src/commonMain/kotlin/com/amegane3231/videosearcher/Platform.kt
+++
@@ -1,5 +0,0 @@
-package com.amegane3231.videosearcher
-
-expect class Platform() {
-    val platform: String
-}
