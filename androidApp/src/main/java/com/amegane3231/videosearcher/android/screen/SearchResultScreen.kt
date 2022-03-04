c2bf25d7733055c59275b81c5804333486386442
amegane <amegane3231@gmail.com>
2022-03-04 20:00:47 +0900

    Change SearchResultScreen

--- androidApp/src/main/java/com/amegane3231/videosearcher/android/screen/SearchResultScreen.kt
+++ androidApp/src/main/java/com/amegane3231/videosearcher/android/screen/SearchResultScreen.kt
@@ -3,20 +3,68 @@ package com.amegane3231.videosearcher.android.screen
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import com.amegane3231.videosearcher.android.R
+import com.amegane3231.videosearcher.android.components.VideoListColumn
+import com.amegane3231.videosearcher.flux.search.SearchAction
+import com.amegane3231.videosearcher.flux.search.SearchStore
+import com.google.accompanist.insets.navigationBarsPadding
+import com.google.accompanist.insets.statusBarsPadding
 
 @Composable
-fun SearchResultScreen() {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        Button(onClick = {}) {
+fun SearchResultScreen(store: SearchStore) {
+    val searchResult by store.youtubeData.collectAsState()
+
+    when (searchResult) {
+        is SearchAction.FetchDataFailed -> {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Text(text = stringResource(R.string.msg_failed_data), textAlign = TextAlign.Center)
+            }
+        }
+        is SearchAction.FetchDataSucceeded -> {
+            VideoListColumn(
+                data = (searchResult as SearchAction.FetchDataSucceeded).data,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .statusBarsPadding()
+                    .navigationBarsPadding()
+            )
+        }
+        is SearchAction.FetchDataWaiting -> {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .statusBarsPadding()
+                    .navigationBarsPadding()
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+        is SearchAction.Standby -> {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .statusBarsPadding()
+                    .navigationBarsPadding()
+            ) {
+                CircularProgressIndicator()
+            }
         }
     }
 }
