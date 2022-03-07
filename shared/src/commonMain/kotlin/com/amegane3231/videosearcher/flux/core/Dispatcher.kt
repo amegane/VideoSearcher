ab13a23b1240592943687d787aa1d44bb03ea7a5
amegane <amegane3231@gmail.com>
2022-03-07 14:54:57 +0900

    Add on method in dispatcher

--- shared/src/commonMain/kotlin/com/amegane3231/videosearcher/flux/core/Dispatcher.kt
+++ shared/src/commonMain/kotlin/com/amegane3231/videosearcher/flux/core/Dispatcher.kt
@@ -1,7 +1,11 @@
 package com.amegane3231.videosearcher.flux.core
 
 import com.badoo.reaktive.observable.Observable
+import com.badoo.reaktive.observable.filter
+import com.badoo.reaktive.observable.map
 import com.badoo.reaktive.subject.publish.PublishSubject
+import io.ktor.util.reflect.instanceOf
+import kotlin.reflect.KClass
 
 object Dispatcher {
     private val _observer = PublishSubject<Action>()
@@ -10,5 +14,9 @@ object Dispatcher {
 
     fun dispatch(action: Action) {
         _observer.onNext(action)
+    }
+
+    inline fun <reified T : Action> on(clazz: KClass<T>): Observable<T> {
+        return observer.filter { it.instanceOf(clazz) }.map { it as T }
     }
 }
