6afee3a49127deff141af7a8aa0dbd8877b6f057
amegane <amegane3231@gmail.com>
2022-03-23 01:13:37 +0900

    Delete dark color theme

--- androidApp/src/main/java/com/amegane3231/videosearcher/android/ui/theme/Theme.kt
+++ androidApp/src/main/java/com/amegane3231/videosearcher/android/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.amegane3231.videosearcher.android.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
@@ -22,21 +21,12 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun MovieSearcherTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
     MaterialTheme(
-        colors = colors,
+        colors = LightColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
     )
 }
-
-object MovieSearcherTheme
