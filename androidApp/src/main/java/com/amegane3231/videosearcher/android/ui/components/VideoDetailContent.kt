0515f8e0c0d4cb820aac147db344787972af4844
amegane <amegane3231@gmail.com>
2022-03-23 01:14:14 +0900

    Change VideoDetail's UI

--- androidApp/src/main/java/com/amegane3231/videosearcher/android/ui/components/VideoDetailContent.kt
+++ androidApp/src/main/java/com/amegane3231/videosearcher/android/ui/components/VideoDetailContent.kt
@@ -1,10 +1,12 @@
 package com.amegane3231.videosearcher.android.ui.components
 
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ModalBottomSheetLayout
 import androidx.compose.material.ModalBottomSheetValue
-import androidx.compose.material.Text
 import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -13,6 +15,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.amegane3231.videosearcher.data.youtube.YoutubeVideoResource
 import com.amegane3231.videosearcher.di.getKoinInstance
 import com.amegane3231.videosearcher.flux.search.action.GetVideoDataAction
@@ -34,12 +37,16 @@ fun VideoDetailContent(
 
     var selectedIndex by remember { mutableStateOf(-1) }
 
-    ModalBottomSheetLayout(sheetState = sheetState, sheetContent = {
-        if (selectedIndex >= 0) {
-            VideoDetailBottomSheetContent(videoDetail)
+    ModalBottomSheetLayout(
+        sheetState = sheetState,
+        sheetShape = RoundedCornerShape(12.dp),
+        sheetContent = {
+            if (selectedIndex >= 0) {
+                VideoDetailBottomSheetContent(videoDetail)
+            }
+            Spacer(modifier = Modifier.height(1.dp))
         }
-        Text(text = "")
-    }) {
+    ) {
         VideoListColumn(
             searchResults = searchResults,
             onClick = {
